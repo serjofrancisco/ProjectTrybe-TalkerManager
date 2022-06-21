6dd081c14b4af1c0acded522965f21f0d0548d6a
Sergio Francisco <spinheiro.lf@gmail.com>
2022-06-21 13:58:26 -0300

    feat: get talkers

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const { read, write } = require('./helpers/filesystem');
+
+app.get('/talker', async (req, res) => {
+    try {
+        const talkers = await read();
+        return res.status(200).json(talkers);
+    } catch (error) {
+        return error;
+    }
+});
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
