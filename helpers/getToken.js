53a3b5b254ccfa6e313aeb97314d0b18f4c5e69c
Sergio Francisco <spinheiro.lf@gmail.com>
2022-06-21 17:00:43 -0300

    requisito 5

--- helpers/getToken.js
+++ helpers/getToken.js
@@ -2,7 +2,7 @@ const getToken = () => {
     const options = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     let token = '';
     for (let i = 0; i < 16; i += 1) {
-      const randomChar = Math.floor(Math.random() * options.length) + 1;
+      const randomChar = Math.floor(Math.random() * options.length);
       token += options[randomChar];
     }
     return token;
